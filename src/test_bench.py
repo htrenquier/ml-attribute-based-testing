569c9575f882b31ebc8d2bb0c261c73a5c39a307
htrenquier <user@Users-MacBook-Pro.local>
2019-05-31 16:32:57 +0900

    Implements color domains experiment

--- src/test_bench.py
+++ src/test_bench.py
@@ -273,7 +273,11 @@ def color_region_finetuning():
 
         model0, model_name0 = mt.train2(m, tr_data, val_data, 'cr_0245', 50, data_augmentation=False, path=res_path)
 
+        train_data_ref = ds.get_data('cifar10', (20000, 30000))
+        model2, model_name2 = mt.fine_tune(model0, model_name0, train_data_ref, val_data, 30, True, 'ref', path=res_path)
+        scores_cube2 = color_domains_accuracy(model2, g)
         # print('score cubes:', scores_cube)
+
 
         for x in xrange(g):
             for y in xrange(g):
@@ -286,14 +290,10 @@ def color_region_finetuning():
                         dselec = np.concatenate((tr_data[0], np.array(operator.itemgetter(*ft_data_args)(ft_data[0]))))
                         dlabels = np.concatenate((tr_data[1], np.array(operator.itemgetter(*ft_data_args)(ft_data[1]))))
                         ft_data_selected = [dselec, dlabels]
-                        train_data_ref = ds.get_data('cifar10', (20000, 30000))
 
-                        model1, model_name1 = mt.fine_tune(model0, model_name0, ft_data_selected, val_data, 20, False,
+                        model1, model_name1 = mt.fine_tune(model0, model_name0, ft_data_selected, val_data, 30, True,
                                                            ft_model_name+'exp', path=res_path)
                         scores_cube1 = color_domains_accuracy(model1, g)
-                        model2, model_name2 = mt.fine_tune(model0, model_name0, train_data_ref, val_data, 20, False,
-                                                           ft_model_name+'ref', path=res_path)
-                        scores_cube2 = color_domains_accuracy(model2, g)
 
                         cc = np.subtract(scores_cube1, scores_cube2)
 
