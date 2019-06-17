ecc3ad36fe1f54acb928b5d35b3b11951dd4701b
htrenquier <user@Users-MacBook-Pro.local>
2019-06-17 13:47:06 +0900

    Training data verification for color domains experiment

--- src/test_bench.py
+++ src/test_bench.py
@@ -17,7 +17,8 @@ os.chdir(os.path.dirname(sys.argv[0]))
 # 'densenet169', 'densenet201',
 # models = ('densenet121', 'mobilenet', 'mobilenetv2', 'nasnet', 'resnet50') #  , 'vgg16', 'vgg19')
 # models = ('densenet121', 'mobilenetv2')
-models = ('mobilenet', 'densenet121', 'densenet169', 'densenet201')
+# models = ('mobilenet', 'densenet121', 'densenet169', 'densenet201')
+models = ['resnet50']
 ilsvrc2012_val_path = '/home/henri/Downloads/imagenet-val/'
 ilsvrc2012_val_labels = '../ilsvrc2012/val_ground_truth.txt'
 ilsvrc2012_path = '../ilsvrc2012/'
@@ -271,7 +272,7 @@ def bug_feature_detection():
         plt.show()
 
         fig, axs = plt.subplots(1, 1)
-        axs.hist(y_predicted2, bins=30)
+        axs.hist(y_predicted2, bins=30, range=(0, 2))
         axs.set_title(m)
         plt.show()
 
@@ -397,6 +398,7 @@ def color_region_finetuning():
                             dlabels = np.concatenate(
                                 (tr_data[1], np.array(operator.itemgetter(*ft_data_args)(ft_data[1]))))
                             ft_data_selected = [dselec, dlabels]
+                            assert len(ft_data_selected[0]) == 10000
                             # Avoid fitting model_base:
                             model0 = mt.load_by_name(model_name0, ft_data[0].shape[1:], res_path + model_name0 + '.h5')
                             # Model state check (should be same acc than base model)
@@ -470,7 +472,7 @@ check_dirs(res_path, ilsvrc2012_path)
 # imagenet_test()
 # finetune_test()
 # data_analysis()
-# bug_feature_detection()
+bug_feature_detection()
 # color_domain()
 # cifar_color_domains_test()
-color_region_finetuning()
+# color_region_finetuning()
